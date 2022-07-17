ce973bae113e07dcba78724bcedc78c11bc37e82
michael simoni <nyjfootball22@aim.com>
2022-07-17 00:53:56 -0400

    update colors

--- backend/src/config.js
+++ backend/src/config.js
@@ -64,7 +64,7 @@ const TOKENS_PER_MINT = 7; // maximum number of NFTs a user can mint in a single
 const PUBLIC_MINT_START_DATE = "2022-07-16T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-07-15T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 700; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x1c1853C597f246051B1dd75C1244a2E2b3D2eff1"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
